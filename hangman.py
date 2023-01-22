95383bdcfc26ec19317178c029fa4e674ac1949d
abagaeva <123305393+abagaeva@users.noreply.github.com>
2023-01-22 19:34:31 +0300

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -6,19 +6,41 @@ def welcome_speech(t):
     Загаданное слово состоит из {len(t)} букв {t}
     ''')
 
-    
+
 def start_template(w):
     return len(w)*['_']
+
+
 def list_to_string_convert(t):
-    pass
+    s = ''
+    for i in t:
+        s += i
+    return s
+
+
+def user_input():
+    return input('Введите букву: ')
+
+
+def build_template(t, w, g=''):
+    for i in range(len(w)):
+        if w[i]== g:
+            t[i] = g
+    return t    
+
+
 def get_word(w):
     return w[0]
-    
 
 
+def check_win(g):
+    for i in g:
+        if i == '_':
+            return True
+    return False
 
 
-
+def check_mistake():
 
 
 
@@ -31,4 +53,17 @@ def game():
     word_in_play = get_word(word)
     template = start_template(word_in_play)
     welcome_speech(list_to_string_convert(template))
-    
+
+
+    while progress:
+        user_guess = user_input()
+        template = build_template(template, word_in_play, user_guess)
+        guessed = list_to_string_convert(template)
+        print(f'Результат: {guessed}')
+        progress = check_win(guessed)
+
+        if not check_mistake(word_in_play, user_guess):
+
+
+
+
